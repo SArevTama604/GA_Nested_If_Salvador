5470583f609690cc86c7e722c1dc89a29d80aa09
Salvador <sarevalotamayo@student.rtc.edu>
2024-02-17 20:49:00 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace GA_Nested_If_Salvador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+            if (age >= 18)
+            {
+                Console.WriteLine(" You are an adult.");
+            }
+            else
+            {
+                Console.WriteLine("You are not an adult.");
+            }
+        }
+    }
+}
